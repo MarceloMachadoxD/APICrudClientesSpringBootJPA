1d610c6b39f0cae30bcaf4fa71d0c7430266e5a8
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-22 09:11:38 -0300

    ClientService dependency injection ClientRepository

--- src/main/java/com/github/marcelomachadoxd/crudclientes/services/ClientService.java
+++ src/main/java/com/github/marcelomachadoxd/crudclientes/services/ClientService.java
@@ -1,9 +1,15 @@
 package com.github.marcelomachadoxd.crudclientes.services;
 
+import com.github.marcelomachadoxd.crudclientes.DTO.ClientDTO;
+import com.github.marcelomachadoxd.crudclientes.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
 
 
 
