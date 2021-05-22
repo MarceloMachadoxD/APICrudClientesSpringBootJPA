a4e266c56a7fed537e77f3003ed888e96ee3affa
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-22 16:47:51 -0300

    Implement exceptions

--- src/main/java/com/github/marcelomachadoxd/crudclientes/services/ClientService.java
+++ src/main/java/com/github/marcelomachadoxd/crudclientes/services/ClientService.java
@@ -3,12 +3,17 @@ package com.github.marcelomachadoxd.crudclientes.services;
 import com.github.marcelomachadoxd.crudclientes.DTO.ClientDTO;
 import com.github.marcelomachadoxd.crudclientes.model.entities.Client;
 import com.github.marcelomachadoxd.crudclientes.repositories.ClientRepository;
+import com.github.marcelomachadoxd.crudclientes.services.exception.DatabaseException;
+import com.github.marcelomachadoxd.crudclientes.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
@@ -19,9 +24,15 @@ public class ClientService {
 
     @Transactional(readOnly = true)
     public ClientDTO findByID(Long id) {
-        Optional<Client> obj = clientRepository.findById(id);
-        Client client = obj.orElseThrow(() -> new RuntimeException("Entity not found with id: " + id));
-        return new ClientDTO(client);
+
+        try {
+            Optional<Client> obj = clientRepository.findById(id);
+            Client client = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found with id: " + id));
+            return new ClientDTO(client);
+        } catch (RuntimeException e){
+            throw new ResourceNotFoundException("id not found " + id);
+        }
+
 
     }
 
@@ -37,8 +48,10 @@ public class ClientService {
     public void deleteClientByID(Long id) {
         try {
             clientRepository.deleteById(id);
-        } catch (RuntimeException e){
-            throw new RuntimeException("Error deleting id " + id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException("Error deleting id " + id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Integrity  violation trying to delete client" + id);
         }
 
     }
@@ -61,8 +74,8 @@ public class ClientService {
             copyDTOtoEntity(clientDTO, client);
 
             return new ClientDTO(client);
-        } catch (RuntimeException e){
-            throw new RuntimeException("Not found id " + clientDTO.getId());
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Not found id " + clientDTO.getId());
         }
 
     }
