91f0136cde4d6a9470bf85071d8d5ceaffdbf8a2
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-22 09:18:39 -0300

    GET by ID

--- src/main/java/com/github/marcelomachadoxd/crudclientes/resource/ClientResource.java
+++ src/main/java/com/github/marcelomachadoxd/crudclientes/resource/ClientResource.java
@@ -1,7 +1,11 @@
 package com.github.marcelomachadoxd.crudclientes.resource;
 
+import com.github.marcelomachadoxd.crudclientes.DTO.ClientDTO;
 import com.github.marcelomachadoxd.crudclientes.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,12 @@ public class ClientResource {
     @Autowired
     private ClientService clientService;
 
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findByID(@PathVariable Long id) {
+        ClientDTO clientDTO = clientService.findByID(id);
+        return ResponseEntity.ok().body(clientDTO);
+    }
 
 
 }
--- src/main/java/com/github/marcelomachadoxd/crudclientes/services/ClientService.java
+++ src/main/java/com/github/marcelomachadoxd/crudclientes/services/ClientService.java
@@ -1,15 +1,27 @@
 package com.github.marcelomachadoxd.crudclientes.services;
 
 import com.github.marcelomachadoxd.crudclientes.DTO.ClientDTO;
+import com.github.marcelomachadoxd.crudclientes.model.entities.Client;
 import com.github.marcelomachadoxd.crudclientes.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
 
     @Autowired
     private ClientRepository clientRepository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findByID(Long id) {
+        Optional<Client> obj = clientRepository.findById(id);
+        Client client = obj.orElseThrow(() -> new RuntimeException("Entity not found with id: " + id));
+        return new ClientDTO(client);
+
+    }
 
 
 
