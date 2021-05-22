0ad144e0e2f9cbde03907a36c738715cbfa67a6d
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-22 09:02:23 -0300

    ClientDTO

---
+++ src/main/java/com/github/marcelomachadoxd/crudclientes/DTO/ClientDTO.java
@@ -0,0 +1,93 @@
+package com.github.marcelomachadoxd.crudclientes.DTO;
+
+import com.github.marcelomachadoxd.crudclientes.model.entities.Client;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class ClientDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private Instant birthDate;
+    private Integer children;
+
+
+    public ClientDTO() {
+    }
+
+    public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client client){
+        this.id = client.getId();
+        this.name = client.getName();
+        this.cpf = client.getCpf();
+        this.income = client.getIncome();
+        this.birthDate = client.getBirtDate();
+        this.children = client.getChildren();
+    }
+
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+}
