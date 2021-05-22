da78dc307b06fa746b23c807c1c21df03e8fca46
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-22 07:53:39 -0300

    Database mock auto seed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+/*
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Alexander Gran Bell', 12585269354, 3524.44, 2, TIMESTAMP WITH TIME ZONE '1920-07-13T20:50:07.12345Z' );
+*/
