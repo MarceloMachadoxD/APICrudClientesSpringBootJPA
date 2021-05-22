4cee7b4a9723ce10479e4d0fad27deb1a3f50424
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-22 08:18:58 -0300

    Increment seed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,1 @@
-/*
-INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Alexander Gran Bell', 12585269354, 3524.44, 2, TIMESTAMP WITH TIME ZONE '1920-07-13T20:50:07.12345Z' );
-*/
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Alexander Gran Bell', '12585269354', 3524.44, 2, TIMESTAMP WITH TIME ZONE '1920-07-13T20:50:07.12345Z' );
